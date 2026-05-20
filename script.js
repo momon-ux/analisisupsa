84fec0710ca5b37e46daea193297c1d3fb314618
Momon <g-11268875@moe-dl.edu.my>
2026-05-20 08:31:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -232,7 +232,16 @@ async function loadDataDariGoogleSheet() {
   }
 }
 
-async function hantarKeGoogleSheet(item) {
+async function async function hantarKeGoogleSheet(item) {
+  await fetch(GOOGLE_SCRIPT_URL, {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8"
+    },
+    body: JSON.stringify(item)
+  });
+}(item) {
   await fetch(GOOGLE_SCRIPT_URL, {
     method: "POST",
     mode: "no-cors",
